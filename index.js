d027a01eaa5d602dfb07750ed2065e82fd1571c6
will <willfoer@gmail.com>
2024-08-20 13:26:07 -0500

    Change paths in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import initScrollReveal from "scripts/scrollReveal";
+import initTiltEffect from "scripts/tiltAnimation";
+import { targetElements, defaultProps } from "data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
